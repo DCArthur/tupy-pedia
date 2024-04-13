478ded2ec064201945cd636e4ee2a6d6db1d097a
imrooteodoro <adelsonteodoro67@gmail.com>
2024-04-12 21:54:06 -0300

    feat: consertando as funcoes de busca

--- src/controllers/dicioControllers.js
+++ src/controllers/dicioControllers.js
@@ -4,28 +4,37 @@ const dicionarioData  = require('../../data/dicionario.json');
 
 //Mostrar dicionario em ordem
 const mostrarDicionario = (req, res) => {
-    const palavrasEmOrdem = bst.inOrder(); 
     lerDicionario()
+    const palavrasEmOrdem = bst.inOrder(); 
     res.render('index', { palavras: palavrasEmOrdem});
+    return {palavras: palavrasEmOrdem}
+
 };
+
 //Mostrar dicionario Pre-ordem
 const dicionarioPreOrdem = (req, res) => {
-    const palavrasPreOrdem = bst.preOrder();
     lerDicionario()
-    res.render('index', {palavras : palavrasPreOrdem});
+    const palavrasPreOrdem = bst.preOrder();
+    res.render('index',  {palavras : palavrasPreOrdem});
 }
+
+
 //Mostra dicionario Pos-ordem
 const dicionarioPosOrdem = (req, res) => {
-    const palavrasPreOrdem = bst.preOrder();
     lerDicionario()
-    res.render('index', {palavras : palavrasPreOrdem});
+    const palavrasPosOrdem = bst.postOrder();
+    res.render('index', {palavras : palavrasPosOrdem});
+    return {palavras: palavrasPosOrdem}
 }
+
+
 
 const lerDicionario = () =>{
     Object.entries(dicionarioData).forEach(([key, value]) => {
         bst.insert(key, value);
     })
 }
+
 
 
 
